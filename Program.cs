762294f234a4de7035a32338cbd808fd289d5b50
nate <natephiliphoffman@proton.me>
2026-04-16 09:08:04 -0400

    adding in wire logic to construct transistors from circuitry

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿
 Console.WriteLine("Hello, World! {0}", Gates.SimpleNAND(false,true));
+
+Wire testWire = new Wire();
+
--- Wire.cs
+++ Wire.cs
@@ -1,18 +1,57 @@
 using System.Reflection.Metadata;
 using ParallelWires = Wire[];
+using SourceWire = Wire;
+using DrainWire = Wire;
+using GateWire = Wire;
 
 public class Wire
 {
-    public ParallelWires[] GroundSerialConnections = [];
+    public event Action<SourceWire, DrainWire[]>? VoltageChange;
+    public event Action<List<byte>>? WireTerminated;
 
-    private void ConnectTransistor()
+    private bool SourceVoltage = false;
+    private bool DrainVoltage = false;
+
+    public ParallelWires ParallelConnectionsNextInSerial = [];
+
+    public Wire GetTransistor(GateWire gate)
     {
-        throw new NotImplementedException();
+        this.VoltageChange += (SourceWire upstreamWire, DrainWire[] downstreamWires) =>
+        {
+            this.SourceVoltage = upstreamWire.DrainVoltage;
+            bool drainVoltage = Transistor(gate.DrainVoltage, this.SourceVoltage);
+            this.DrainVoltage = drainVoltage;
+
+            if(downstreamWires.Length > 0) updateVoltageOnWires(downstreamWires);
+            else WireTerminated?.Invoke();
+        };
+
+        return this;
     }
+
+    private void updateVoltageOnWires(Wire[] downstreamWires)
+    {
+        downstreamWires.ToList().ForEach((wire) =>
+        {
+            wire.VoltageChange?.Invoke(this, ParallelConnectionsNextInSerial);
+        });
+    }
+
+    public void SetVoltage(bool voltage)
+    {
+        this.SourceVoltage = voltage;
+        updateVoltageOnWires(ParallelConnectionsNextInSerial);
+    }
+
+    public static List<byte> GetBinaryOutput(Wire[] wires)
+    {
+        List<byte> bytes = wires.ToList().Select((wire) => Convert.ToByte(wire.DrainVoltage)).ToList();
+        return bytes;
+    } 
 
     private static bool Transistor(bool gate, bool source)
     {
-        // drain is the downstream line on the transistor
+        // drain is the downstream line on the transistor, gate opens and closes it, source is incoming voltage
         bool drain = gate && source;
         return drain;
     }
