da37413db283274038bd6e3ef29b7ac088fbb136
nate <natephiliphoffman@proton.me>
2026-04-28 13:14:05 -0400

    updating nand logic to be closer to a cpu

--- Gate/NAND.cs
+++ Gate/NAND.cs
@@ -2,26 +2,26 @@ using System.Runtime.CompilerServices;
 
 public static class Gate
 {
-    // this isn't exactly how cpu nands work, but it is a nand, think about this more like a hobbyist nand
-    //  real nands have to worry about constant current and voltage regulation (and voltage direction)
+    // CMOS-style NAND: PMOS parallel pull-up, NMOS series pull-down
     public static WireBase BuildNAND(WireBase input1, WireBase input2)
     {
-        var power = new StraightWire();
-        
+        var power  = new StraightWire();
+        var ground = new StraightWire();
+        var output = new StraightWire();
+
         var pmos1 = new PMOSTransistor() << input1;
         var pmos2 = new PMOSTransistor() << input2;
-
-        var pmosSerial = power > (pmos1 + pmos2);
-
-        var pmosPath = pmosSerial > new StraightWire();
+        var pmosPath = power > (pmos1 + pmos2);
+        _ = pmosPath > output;
 
         var nmos1 = new NMOSTransistor() << input1;
         var nmos2 = new NMOSTransistor() << input2;
-
-        var nmosPath = power > nmos1 > nmos2;
+        var nmosPath = ground > nmos1 > nmos2;
+        _ = nmosPath > output;
 
         power.SetVoltage(true);
- 
-        return nmosPath;
+        ground.SetVoltage(false);
+
+        return output;
     }
 }
