6c4b81fb0232bdd61d06f650645a53756cd7e3ee
nate <natephiliphoffman@proton.me>
2026-05-12 17:28:02 -0400

    added adder logic

--- Circuits/Adder.cs
+++ Circuits/Adder.cs
@@ -1,8 +1,16 @@
-public class Adder {
+public static class Adder {
     
-    public void HalfAdder()
+    public static ValueTuple<WireBase, WireBase> HalfAdder(WireBase a, WireBase b)
     {
-        
+        WireBase N1 = Gate.BuildNAND(a, b);
+        WireBase N2 = Gate.BuildNAND(a, N1);
+        WireBase N3 = Gate.BuildNAND(b, N1);
+
+        WireBase sum = Gate.BuildNAND(N2, N3);
+        WireBase carry = Gate.BuildNAND(N1, N1);
+
+        return (sum, carry);
+
     }
 
 }
